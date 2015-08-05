e55646329f97e87253e3e5bd0d33a51a000afbb2
PuZZleDucK <PuZZleDucK@gmail.com>
2015-08-05 16:57:17 +1000

    added git config script

---
+++ config-git-puzzle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export GIT_COMMITTER_EMAIL=PuZZleDucK@Gmail.com
+export GIT_COMMITTER_NAME='PuZZleDucK'
+export GIT_AUTHOR_EMAIL=PuZZleDucK@gmail.com
+export GIT_AUTHOR_NAME='Ben Minerds'
+
+git config --global user.email "PuZZleDucK@gmail.com"
+git config --global user.name "PuZZleDucK"
