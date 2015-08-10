4bc3382e9d491f7f4f76c0c21673b70799309d32
PuZZleDucK <PuZZleDucK@gmail.com>
2015-08-10 10:35:39 +1000

    prototype save function

---
+++ save-config.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+
+GIT_REPO="https://github.com/PuZZleDucK/Tooling.git"
+
+echo " == Saving BootStrap Config == "
+echo "User: $USER"
+echo "Pushing to: $GIT_REPO"
+echo "Git Dir: $HOME/x/Tooling/"
+echo "Commit message: $@"
+echo ""
+
+read -p "<ctrl>-c to cancel, enter to proceed..."
+
+if [ -d "$HOME/x/Tooling" ]; then
+  echo "Git dir found"
+else
+  echo "Error"
+fi
+
+
+# Xmonad
+cp "$HOME/.db-status.sh" "$HOME/x/Tooling/home/"
+cp "$HOME/.volume.sh" "$HOME/x/Tooling/home/"
+cp "$HOME/.xmobarrc" "$HOME/x/Tooling/home/"
+cp "$HOME/.xmobarrc2" "$HOME/x/Tooling/home/"
+cp "$HOME/.xmonad/xmonad.hs" "$HOME/x/Tooling/home/.xmonad/"
+
+
+
+# Commit update
+echo "commit... TODO"
+
+# Push update
+echo "push... TODO"
+
+
+
