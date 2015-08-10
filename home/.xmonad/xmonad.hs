7672e9874011c78c167f97e89b58c011c99c8fba
PuZZleDucK <PuZZleDucK@gmail.com>
2015-08-10 15:06:54 +1000

    tweaks

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -15,17 +15,19 @@ myBarToo = "xmobar ~/.xmobarrc2"
 -- Custom PP, configure it as you like. It determines what is being written to the bar.
 myPP = xmobarPP { ppCurrent = xmobarColor "yellow" "" . wrap "|" "|"
                 , ppTitle = xmobarColor "yellow" "" . shorten 150
+                , ppSep = "<fc=#00FF00> | </fc>"
                 }
 
 -- Key binding to toggle the gap for the bar.
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myConfig = defaultConfig { 
-          modMask = mod4Mask --i've gotten used to <alt>
-        , workspaces = ["1_email","2_","3_","4_","5_code","6_","7_","8_","9_media","0_","-_","=_"]
+          modMask = mod4Mask
+        , workspaces = ["1_ff","2_","3_work","4_over","5_code","6_","7_","8_","9_vlc","0_","-_","=_"]
         , terminal = "gnome-terminal"
         , borderWidth = 3
-        , focusedBorderColor = "yellow"
+        , focusedBorderColor = "#FFFF00"
+        , normalBorderColor = "#444400"
         , manageHook = myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
   } `additionalKeys`
@@ -39,8 +41,8 @@ myConfig = defaultConfig {
 
 
 myManageHook = composeAll
-   [ className =? "Firefox" --> doShift "1_email"
-     , className =? "vlc" --> doShift "9_media"
+   [ className =? "Firefox" --> doShift "1_ff"
+     , className =? "vlc" --> doShift "9_vlc"
      , className =? "nemo" --> doShift "5_code"
      , manageDocks
    ]
