1427f2b564dbc99246f7be458358f8683fe855ed
puzzleduck <puzzleduck@gmail.com>
2015-11-03 10:49:48 +1100

    Add --yes flag to stop user prompts

--- config-apt-puzzle.sh
+++ config-apt-puzzle.sh
@@ -6,7 +6,7 @@ read -p "Instaling packages from 'program-list.txt'. <ctrl>-c to cancel..."
 for line in `cat program-list.txt` ; do
 #  if [[ "$line" != "" ]] ; then
     echo -e "\nINSTALLING: $line"
-    sudo apt-get install $line
+    sudo apt-get install --yes $line
 #  fi
 done
 
